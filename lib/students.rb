f1ed631c6434fb191cd59427ca37ae4f599b68f2
Karl Benbeneck <kbenbeneck@gmail.com>
2020-03-18 04:14:00 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,7 +1,7 @@
 ## Code your solution below. Note that your SQL queries should be in quotation marks.
 
-def highest_student_gp
-  "SELECT COUNT(gpa) FROM students;"
+def highest_student_gpa
+  "SELECT MAX(gpa) FROM students;"
 end
 
 def lowest_student_gpa
